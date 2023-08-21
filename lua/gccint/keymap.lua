b7c5d225de327f95707c714f300e67ed83c1f491
gccintra <gustavo_cintra@trimble.com>
2023-08-21 15:08:57 -0300

    fix(buffers): change the command used for moving buffers

--- lua/gccint/keymap.lua
+++ lua/gccint/keymap.lua
@@ -18,8 +18,12 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", ":BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", ":BufferLineCyclePrev<CR>", opts)
+
+-- Move buffers
+keymap("n", "<C-l>", ":BufferLineMoveNext<CR>", opts)
+keymap("n", "<C-h>", ":BufferLineMovePrev<CR>", opts)
 
 -- Close buffers
 keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
@@ -36,7 +40,30 @@ keymap("v", "<leader>y", '"+y', opts)
 keymap("n", "<leader>Y", '"+Y', opts)
 
 -- Substitute
-keymap("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", opts)
+keymap(
+    "n",
+    "<leader>s",
+    ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+    opts
+)
+
+-- Find and replace in all .h, .cpp files of the current directory
+keymap(
+    "n",
+    "<leader><leader>s",
+    ":argdo %s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+    opts
+)
+
+keymap(
+    "n",
+    "<leader>cr",
+    ":s/^/\\/\\/ Copyright (c) 2023 Trimble Transportation. All rights reserved. Trimble and\\r\\/\\/ the Globe \\& Triangle Logo are trademarks of Trimble Inc., registered in the\\r\\/\\/ United States Patent and Trademark Office and\\/or in other countries.\\r\\r<cr>",
+    opts
+)
+
+-- Clangd switch header/implementation
+keymap("n", "<space>", ":ClangdSwitchSourceHeader<CR>", opts)
 
 -- NvimTree
 keymap("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", opts)
@@ -58,14 +85,15 @@ keymap("x", "<C-_>", function()
 end, opts)
 
 -- DAP
-keymap("n", "<leader>db", require("dap").toggle_breakpoint, opts)
-keymap("n", "<leader>dc", require("dap").continue, opts)
-keymap("n", "<leader>di", require("dap").step_into, opts)
-keymap("n", "<leader>do", require("dap").step_over, opts)
-keymap("n", "<leader>dO", require("dap").step_out, opts)
-keymap("n", "<leader>dr", require("dap").repl.toggle, opts)
-keymap("n", "<leader>dl", require("dap").run_last, opts)
+local dap = require "dap"
+keymap("n", "<leader>db", dap.toggle_breakpoint, opts)
+keymap("n", "<leader>dc", dap.continue, opts)
+keymap("n", "<leader>di", dap.step_into, opts)
+keymap("n", "<leader>do", dap.step_over, opts)
+keymap("n", "<leader>dO", dap.step_out, opts)
+keymap("n", "<leader>dr", dap.repl.toggle, opts)
+keymap("n", "<leader>dl", dap.run_last, opts)
+keymap("n", "<leader>dt", dap.terminate, opts)
 keymap("n", "<leader>du", require("dapui").toggle, opts)
-keymap("n", "<leader>dt", require("dap").terminate, opts)
 
 keymap("n", "<leader>cp", require("copilot.panel").open, opts)
